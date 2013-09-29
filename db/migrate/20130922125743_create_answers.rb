1f752520a90b87ba97a77934e292a38e7eceb602
Thorpe J.Y. Obazee <forum.providence@yahoo.com>
2013-09-29 18:04:38 +0800

    text

--- db/migrate/20130922125743_create_answers.rb
+++ db/migrate/20130922125743_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.string :question_id
-      t.string :body
+      t.text :body
       t.string :user_id
 
       t.timestamps
